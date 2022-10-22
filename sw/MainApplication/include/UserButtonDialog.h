e73a5d4e602f29e7f7ded977dd834dd41a412423
Jacek Skowronek <jacek.skowronek@aptiv.com>
2022-10-22 17:39:10 +0200

    UserButtonDialog added

---
+++ sw/MainApplication/include/UserButtonDialog.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <stdint.h>
+#include <optional>
+
+#include <QtCore/QObject>
+#include <QtWidgets/QDialog>
+#include <QtWidgets/QFormLayout>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QDialogButtonBox>
+#include <QtWidgets/QTextEdit>
+
+
+class UserButtonDialog : public QObject
+{
+   Q_OBJECT
+
+public:
+   UserButtonDialog();
+   ~UserButtonDialog();
+
+   struct Settings
+   {
+      std::string button_name;
+      std::string raw_commands;
+      std::vector<std::string> commands;
+   };
+
+   std::optional<bool> showDialog(QWidget* parent, const Settings& current_settings, Settings& out_settings, bool allow_edit);
+private:
+   QDialog* m_dialog;
+   QFormLayout* m_form;
+   QDialogButtonBox* m_buttonBox;
+   QLineEdit* m_buttonNameEdit;
+   QTextEdit* m_commandEdit;
+   bool m_editable;
+
+   void addDialogButtons();
+   bool convertGuiValues(Settings& out_settings);
+};
---
+++ sw/MainApplication/source/UserButtonDialog.cpp
@@ -0,0 +1,63 @@
+#include "UserButtonDialog.h"
+#include "Logger.h"
+
+UserButtonDialog::UserButtonDialog():
+m_dialog(nullptr),
+m_form(nullptr),
+m_buttonBox(nullptr),
+m_buttonNameEdit(nullptr),
+m_commandEdit(nullptr),
+m_editable(true)
+{
+}
+UserButtonDialog::~UserButtonDialog()
+{
+
+}
+std::optional<bool> UserButtonDialog::showDialog(QWidget* parent, const Settings& current_settings, Settings& out_settings, bool allow_edit)
+{
+   std::optional<bool> result;
+   m_dialog = new QDialog(parent);
+   m_form = new QFormLayout(m_dialog);
+   m_editable = allow_edit;
+
+   m_buttonNameEdit = new QLineEdit(m_dialog);
+   m_buttonNameEdit->setText(QString(current_settings.button_name.c_str()));
+   m_buttonNameEdit->setEnabled(allow_edit);
+   m_form->addRow(m_buttonNameEdit);
+
+   m_commandEdit = new QTextEdit(m_dialog);
+   m_commandEdit->setText(QString(current_settings.raw_commands.c_str()));
+   m_commandEdit->setEnabled(allow_edit);
+   m_form->addRow(m_commandEdit);
+
+   addDialogButtons();
+
+   m_dialog->setWindowModality(Qt::ApplicationModal);
+   UT_Log(MAIN_GUI, INFO, "Command button dialog opened");
+   if (m_dialog->exec() == QDialog::Accepted)
+   {
+      UT_Log(MAIN_GUI, HIGH, "dialog accepted, gathering new settings");
+      result = convertGuiValues(out_settings);
+   }
+
+   delete m_form;
+   delete m_dialog;
+
+   return result;
+}
+
+void UserButtonDialog::addDialogButtons()
+{
+   m_buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, Qt::Horizontal, m_dialog);
+   m_buttonBox->setDisabled(!m_editable);
+   m_form->addWidget(m_buttonBox);
+   QObject::connect(m_buttonBox, SIGNAL(accepted()), m_dialog, SLOT(accept()));
+   QObject::connect(m_buttonBox, SIGNAL(rejected()), m_dialog, SLOT(reject()));
+}
+bool UserButtonDialog::convertGuiValues(Settings& out_settings)
+{
+   out_settings.button_name = m_buttonNameEdit->text().toStdString();
+   out_settings.raw_commands = m_commandEdit->toPlainText().toStdString();
+   return true;
+}
