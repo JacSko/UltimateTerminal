3f92f4605cd0675907b50659fd95936d91f14e40
Jacek Skowronek <jacek.skowronek@aptiv.com>
2022-11-03 22:25:01 +0100

    add run_coverage.sh script to calculate coverage

---
+++ run_coverage.sh
@@ -0,0 +1,12 @@
+./run_ut.sh
+
+cd build_ut
+
+if [[ ! -d "html/" ]]
+then
+    echo "creating html dir"
+    mkdir html
+fi
+
+gcovr -f ../sw/ --root=. -e /.*/test/.* -e /.*/tests/.* --html --html-details --output=html/coverage.html --exclude-unreachable-branches
+firefox html/coverage.html
