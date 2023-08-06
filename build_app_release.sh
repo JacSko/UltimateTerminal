b3b47f450f31430d1c1f526ffaa16ae5604f2e12
Jacek Skowronek <jacek.skowronek@aptiv.com>
2023-08-06 17:43:00 +0200

    Deploy builds app in release version

---
+++ build_app_release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash -e
+
+CUSTOM_EXECUTOR_NUMBER=8
+CUSTOM_CMAKE_PARAMETERS="-DCMAKE_BUILD_TYPE=Release"
+
+mkdir -p build_release
+pushd build_release
+
+cmake .. ${CUSTOM_CMAKE_PARAMETERS}
+
+if [[ $? -eq 0 ]];
+then
+   echo "=============================================="
+   echo "=== Project configured correctly, building ==="
+   echo "=============================================="
+   make -j${CUSTOM_EXECUTOR_NUMBER}
+   if [[ $? -eq 0 ]];
+   then
+      echo "=========================="
+      echo "=== Build successfully ==="
+      echo "=========================="
+   else
+      echo "=========================="
+      echo "====== Build error! ======"
+      echo "=========================="
+   fi
+else
+      echo "=========================="
+      echo "====== Cmake error! ======"
+      echo "=========================="
+fi
+
+popd
--- deploy_app.sh
+++ deploy_app.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
-./build_app.sh
+./build_app_release.sh
 
 if [[ $? -eq 0 ]];
 then
    echo " Build correctly, preparing deployment package"
    mkdir -p deploy
    pushd deploy
-   cp ../build/bin/UltimateTerminal .
-   cp ../build/bin/.system_config.json .
+   cp ../build_release/bin/UltimateTerminal .
+   cp ../build_release/bin/.system_config.json .
    cp /usr/lib/x86_64-linux-gnu/libQt5Widgets.so.5 .
    cp /usr/lib/x86_64-linux-gnu/libQt5Gui.so.5 .
    cp /usr/lib/x86_64-linux-gnu/libQt5Core.so.5 .
